3da9f162972559261414cd830565e6aeea69a2ed
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2025-03-13 21:22:36 +0700

    [Update]
    bịch bóng, con chim phía trên, còn lại ở dưới
    tất cả đều phải nhanh sau 30s

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -3,25 +3,14 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed;
-    private PlayerController playerControllerScript;
-    //private float leftBound = -15;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (!playerControllerScript.gameover)
-        //{
-            
-        //}
         transform.Translate(Vector3.left * Time.deltaTime * speed);
-        //if (transform.position.x<leftBound && gameObject.CompareTag("Obstacle"))
-        //{
-        //    Destroy(gameObject);
-        //}
+    }
+
+    public void IncreaseSpeed(float amount)
+    {
+        speed += amount;
     }
 }
