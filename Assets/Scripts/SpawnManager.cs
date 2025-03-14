7b6201f25dcca72423c85a6e461e21454d675600
ngwyren <ngwynhtandinh@gmail.com>
2025-03-14 15:10:26 +0700

    Revert "[Update]"
    This reverts commit 043eaa636357626ea641489345adb7912a41aa7f.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -24,37 +24,7 @@ public class SpawnManager : MonoBehaviour
     {
         if (!playerControllerScript.gameover)
         {
-            var spawnPos = new Vector3(10, Random.Range(-1, 2), 0);
-            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
-        }
-        else
-        {
-            var spawnPos = new Vector3(10, Random.Range(-2.5f, -3.5f), 0);
-            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
-        }
-
-        if (checkSpeed)
-        {
-            IncreaseAllMoveLeftSpeed(3);
-            checkSpeed = false;
-        }
-    }
-
-    private IEnumerator IncreaseSpeedOverTime()
-    {
-        while (!playerControllerScript.gameover)
-        {
-            yield return new WaitForSeconds(40); // Wait for 40 seconds
-            checkSpeed = true;
-        }
-    }
-
-    private void IncreaseAllMoveLeftSpeed(float amount)
-    {
-        var moveLeftScripts = FindObjectsByType<MoveLeft>(FindObjectsSortMode.None);
-        foreach (var moveLeft in moveLeftScripts)
-        {
-            moveLeft.IncreaseSpeed(amount);
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
         }
     }
 }
