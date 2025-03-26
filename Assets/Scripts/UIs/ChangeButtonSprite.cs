556aa0fd0bb52ae475ac9e6644ee092720429ab5
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2025-03-26 19:45:42 +0700

    [Update] fix button volume

--- Assets/Scripts/UIs/ChangeButtonSprite.cs
+++ Assets/Scripts/UIs/ChangeButtonSprite.cs
@@ -4,76 +4,82 @@ using UnityEngine.UI;
 
 public class ChangeButtonSprite : MonoBehaviour
 {
-    public Sprite normalSprite; // Sprite bình thường
-    public Sprite pressedSprite; // Sprite khi nhấn
-    private Button button;
-    private Image buttonImage;
+    [SerializeField] private Sprite m_normalSprite;
+    [SerializeField] private Sprite m_pressedSprite;
+
+    private Button m_button;
+    private Image m_buttonImage;
     private VolumeSettings m_volumeSettings;
+    private bool m_isVFXButton;
+    private bool m_isSoundButton;
 
     void Start()
     {
+        // Get components
         m_volumeSettings = FindFirstObjectByType<VolumeSettings>();
-        button = GetComponent<Button>();
-        buttonImage = GetComponent<Image>();
+        m_button = GetComponent<Button>();
+        m_buttonImage = GetComponent<Image>();
 
-        if (gameObject.name.Contains("VFXButton"))
+        // Cache button type
+        string buttonName = gameObject.name;
+        m_isVFXButton = buttonName.Contains("VFXButton");
+        m_isSoundButton = buttonName.Contains("SoundButton");
+
+        // Set initial sprite state
+        UpdateButtonSprite();
+
+        // Add click listener
+        m_button.onClick.AddListener(ToggleSprite);
+    }
+
+    void Update()
+    {
+        // Only update sprite if needed
+        UpdateButtonSprite();
+    }
+
+    private void UpdateButtonSprite()
+    {
+        if (m_isVFXButton)
         {
-            if (m_volumeSettings.GetVFXVolume() == 0)
-            {
-                buttonImage.sprite = pressedSprite;
-            }
-            else
-            {
-                buttonImage.sprite = normalSprite;
-            }
+            m_buttonImage.sprite = m_volumeSettings.GetVFXVolume() > 0 ? m_normalSprite : m_pressedSprite;
         }
-
-        if (gameObject.name.Contains("SoundButton"))
+        else if (m_isSoundButton)
         {
-            Debug.Log(m_volumeSettings.GetMusicVolume());
-            if (m_volumeSettings.GetMusicVolume() == 0)
-            {
-                buttonImage.sprite = pressedSprite;
-            }
-            else
-            {
-                buttonImage.sprite = normalSprite;
-            }
+            m_buttonImage.sprite = m_volumeSettings.GetMusicVolume() > 0 ? m_normalSprite : m_pressedSprite;
         }
-
-        button.onClick.AddListener(ToggleSprite);
-
     }
 
     void ToggleSprite()
     {
-        if (gameObject.name == "VFXButton")
+        if (m_isVFXButton)
         {
-            if (buttonImage.sprite == normalSprite)
+            bool isMuted = m_buttonImage.sprite == m_normalSprite;
+            m_buttonImage.sprite = isMuted ? m_pressedSprite : m_normalSprite;
+
+            if (isMuted)
             {
                 AudioManager.Instance.PlayButtonSound();
-                buttonImage.sprite = pressedSprite;
                 m_volumeSettings.MuteVFX();
             }
             else
             {
-                buttonImage.sprite = normalSprite;
                 m_volumeSettings.UnmuteVFX();
                 AudioManager.Instance.PlayButtonSound();
             }
         }
-
-        if (gameObject.name == "SoundButton")
+        else if (m_isSoundButton)
         {
-            if (buttonImage.sprite == normalSprite)
+            bool isMuted = m_buttonImage.sprite == m_normalSprite;
+            m_buttonImage.sprite = isMuted ? m_pressedSprite : m_normalSprite;
+
+            if (isMuted)
             {
-                buttonImage.sprite = pressedSprite;
                 m_volumeSettings.MuteMusic();
                 AudioManager.Instance.PlayButtonSound();
             }
             else
             {
-                buttonImage.sprite = normalSprite;
                 m_volumeSettings.UnmuteMusic();
                 AudioManager.Instance.PlayButtonSound();
             }
